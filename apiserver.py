d605847acebd8b4d856e230234bd83e72a1d2cf0
cosmicc <ianperry99@gmail.com>
2023-05-13 22:02:14 -0400

    updates

--- apiserver.py
+++ apiserver.py
@@ -1,12 +1,32 @@
+import numpy as np
 import psycopg2
+import configparser
 from flask import Flask, jsonify, request, abort
 import datetime
+import pytz
+
+DEBUG = True
+
+config = configparser.ConfigParser()
+config.read('/etc/growbot.ini')
 
 app = Flask(__name__)
-ALLOWED_USER_AGENT = "ESP32HTTPClient"
+API_PORT = config['api']['port']
+ALLOWED_USER_AGENT = config['api']['allowed_user_agent']
+ALLOWED_HOST = config['api']['allowed_host']
+POSTGRESQL_HOST = config['postgresql']['host']
+POSTGRESQL_ADMIN_DATABASE = config['postgresql']['admin_database']
+POSTGRESQL_ADMIN_USERNAME = config['postgresql']['admin_user']
+POSTGRESQL_ADMIN_PASSWORD = config['postgresql']['admin_password']
+POSTGRESQL_DATABASE = config['postgresql']['database']
+POSTGRESQL_USERNAME = config['postgresql']['user']
+POSTGRESQL_PASSWORD = config['postgresql']['password']
+
+SENSOR_LOW = 1100  # Wet
+SENSOR_HIGH = 2400 # Dry
 
 # Connect to the PostgreSQL database
-conn = psycopg2.connect(host="localhost",database="postgres",user="postgres",password="Whatsthe411!!")
+conn = psycopg2.connect(host=POSTGRESQL_HOST,database=POSTGRESQL_ADMIN_DATABASE,user=POSTGRESQL_ADMIN_USERNAME,password=POSTGRESQL_ADMIN_PASSWORD)
 cur = conn.cursor()
 
 # Create the database if it doesn't exist
@@ -16,25 +36,60 @@ cur.close()
 conn.close()
 
 # Connect to the database
-conn = psycopg2.connect(host="localhost",database="garden_data", user="endpoint", password="Ifa6wasa9")
+conn2 =psycopg2.connect(host=POSTGRESQL_HOST,database=POSTGRESQL_DATABASE, user=POSTGRESQL_USERNAME, password=POSTGRESQL_PASSWORD)
+conn3 = psycopg2.connect(host=POSTGRESQL_HOST,database=POSTGRESQL_DATABASE, user=POSTGRESQL_USERNAME, password=POSTGRESQL_PASSWORD)
 
 # Create the table if it doesn't exist
-cur = conn.cursor()
-cur.execute("SELECT to_regclass('sensor_data')")
-exists = cur.fetchone()[0]
+cur2 = conn2.cursor()
+cur2.execute("SELECT to_regclass('sensor_data')")
+exists = cur2.fetchone()[0]
 if not exists:
-    print("sensor_data table is missing, creating table")
-    cur.execute("CREATE TABLE sensor_data (id SERIAL PRIMARY KEY, device_id VARCHAR(4), sensor_id INTEGER, soil_value INTEGER, soil_pct INTEGER, status_bit VARCHAR(1), batt_volt NUMERIC(2,1), batt_pct INTEGER, timestamp TIMESTAMP, arrived TIMESTAMP, sys_error VARCHAR(32))")
+    print("(!) Sensor_data table is missing, creating table")
+    cur2.execute("CREATE TABLE sensor_data (id SERIAL PRIMARY KEY, device_id VARCHAR(4), sensor_id INTEGER, sensor_name VARCHAR(255), soil_value INTEGER, soil_pct INTEGER, status_bit VARCHAR(1), batt_volt NUMERIC(3,2), batt_pct INTEGER, timestamp TIMESTAMP, arrived TIMESTAMP, version INTEGER, sys_error VARCHAR(32))")
 
-conn.commit()
-cur.close()
+conn2.commit()
+cur2.close()
+
+def convert_timezone(time_str):
+    # Convert string to datetime object
+    time_utc = datetime.datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+    # Set the timezone for the datetime object
+    timezone_utc = pytz.timezone('UTC')
+    time_utc = timezone_utc.localize(time_utc)
+    # Convert UTC datetime to ETC timezone datetime
+    timezone_etc = pytz.timezone('US/Eastern')
+    time_etc = time_utc.astimezone(timezone_etc)
+    # Convert datetime object back to string
+    time_str_etc = time_etc.strftime('%Y-%m-%d %H:%M:%S')
+    return time_str_etc
+
+def update_sensor_name(name, device_id, sensor_id):
+    cur3 = conn3.cursor()
+    cur3.execute(f"UPDATE sensor_names SET sensor_name = '{name}' WHERE device_id = 'device_id' AND sensor_id = {sensor_id}")
+    conn3.commit()
+    cur3.close()
+
+def get_sensor_name(device_id, sensor_id):
+    cur3 = conn3.cursor()
+    print(f"(+) Getting sensor name for device_id: {device_id} and sensor_id: {sensor_id}")
+    cur3.execute(f"SELECT sensor_name FROM sensor_names WHERE device_id = '{device_id}' AND sensor_id = {sensor_id}")
+    data = cur3.fetchone()
+    if (data is not None):
+        data = data[0]
+    else:
+        data = "Unlabeled"
+    print(f'(+) Sensor name retrieved for device_id: {device_id} and sensor_id: {sensor_id}: {data}')
+    cur3.close()
+    return data
 
 @app.before_request
 def check_user_agent():
     user_agent = request.headers.get("User-Agent")
-    print(f"Incoming request user_agent: {user_agent}")
+    print(f"(+) Incoming request user_agent: {user_agent}")
     if user_agent != ALLOWED_USER_AGENT:
-        abort(403)
+        return "You're not allowed to access this resource", 403
+    if request.remote_addr != ALLOWED_HOST:
+        return "You're not allowed to access this resource", 403
 
 # Define a simple endpoint
 @app.route('/ping', methods=['GET'])
@@ -45,25 +100,28 @@ def hello():
 @app.route('/sensor', methods=['POST'])
 def insert_data():
     data = request.get_json()
-    print(f"Incoming Sensor Data: {data}")
+    print(f"(+) Incoming Sensor Data: {data}")
     device_id = data.get('device_id')
     sensor_id = data.get('sensor_id')
     soil_value = data.get('soil_value')
-    soil_pct = data.get('soil_pct')
+    soil_pct = int(np.interp(soil_value, [SENSOR_LOW, SENSOR_HIGH], [100, 0]))
     status_bit = data.get('status_bit')
     batt_volt = data.get('batt_volt')
     batt_pct = data.get('batt_pct')
-    timestamp = data.get('timestamp')
+    timestamp = convert_timezone(data.get('timestamp'))
     reason = data.get('reason')
+    version = data.get('version')
+    #version = 2
+    sensor_name = get_sensor_name(device_id, sensor_id)
     now = datetime.datetime.now()
     arrived = now.strftime('%Y-%m-%d %H:%M:%S')
-    cur = conn.cursor()
-    cur.execute(f"INSERT INTO sensor_data (device_id, sensor_id, soil_value, soil_pct, status_bit, batt_volt, batt_pct, timestamp, arrived, sys_error) VALUES ('{device_id}', {sensor_id}, {soil_value}, {soil_pct}, '{status_bit}', '{batt_volt}', {batt_pct}, '{timestamp}', '{arrived}', '{reason}')")
-    conn.commit()
-    cur.close()
+    print(f"(+) INSERTING DATA: '{device_id}', {sensor_id}, '{sensor_name}', {soil_value}, {soil_pct}, '{status_bit}', '{batt_volt}', {batt_pct}, '{timestamp}', '{arrived}', {version}, '{reason}'")
+    cur2 = conn2.cursor()
+    cur2.execute(f"INSERT INTO sensor_data (device_id, sensor_id, sensor_name, soil_value, soil_pct, status_bit, batt_volt, batt_pct, timestamp, arrived, version, sys_error) VALUES ('{device_id}', {sensor_id}, '{sensor_name}', {soil_value}, {soil_pct}, '{status_bit}', '{batt_volt}', {batt_pct}, '{timestamp}', '{arrived}', {version}, '{reason}')")
+    conn2.commit()
+    cur2.close()
     return jsonify({'response': 'Sensor data accepted.'})
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0', port=4505, debug=True)
+    app.run(host='0.0.0.0', port=API_PORT, debug=DEBUG)
     conn.close()
--- dashboard.py
+++ dashboard.py
@@ -1,43 +1,71 @@
-from flask import Flask, render_template
+import pandas as pd
+import plotly.express as px
+import configparser
+from flask import Flask, request, render_template
 import psycopg2
+from datetime import datetime, timedelta
+
+DEBUG = True
 
 app = Flask(__name__)
 
-# Database connection configuration
-DB_HOST = "localhost"
-DB_NAME = "garden_data"
-DB_USER = "enduser"
-DB_PASS = "Ifa6wasa9"
+# read config settings from /etc/growbot.ini
+config = configparser.ConfigParser()
+config.read('/etc/growbot.ini')
+DASHBOARD_PORT = config['dashboard']['port']
+ALLOWED_HOST = config['api']['allowed_host']
+DB_HOST = config['postgresql']['host']
+DB_NAME = config['postgresql']['database']
+DB_USER = config['postgresql']['user']
+DB_PASS = config['postgresql']['password']
 
-# Retrieve sensor data from the database
+def connect_database():
+    conn = psycopg2.connect(host=DB_HOST,dbname=DB_NAME,user=DB_USER,password=DB_PASS)
+    return conn
+
+def update_sensor_name(name, device_id, sensor_id):
+    conn = connect_database()
+    cur = conn.cursor()
+    cur.execute(f"UPDATE sensor_names SET sensor_name = '{name}' WHERE device_id = 'device_id' AND sensor_id = {sensor_id}")
+    conn.commit()
+    cur.close()
+    conn.close()
+
+def get_sensor_name(device_id, sensor_id):
+    conn = connect_database()
+    cur = conn.cursor()
+    cur.execute(f"SELECT sensor_name FROM sensor_names WHERE device_id = 'device_id' AND sensor_id = {sensor_id}")
+    sensor_name = cur.fetchone()
+    cur.close()
+    conn.close()
+    return sensor_name
+
+from datetime import datetime, timedelta
+
+def is_past_six_hours(datetime_obj):
+    # Get the current datetime
+    current_datetime = datetime.now()
+    # Calculate the difference between the input datetime and the current datetime
+    time_diff = current_datetime - datetime_obj
+    # Check if the time difference is more than 6 hours
+    if time_diff > timedelta(hours=6):
+        return True
+    else:
+        return False
+
+# Retrieve the latest sensor data for each device/sensor combination
 def get_sensor_data():
-    conn = psycopg2.connect(
-        host=DB_HOST,
-        dbname=DB_NAME,
-        user=DB_USER,
-        password=DB_PASS
-    )
+    conn = connect_database()
     cur = conn.cursor()
     cur.execute("""
-        SELECT *
+        SELECT DISTINCT ON (device_id, sensor_id) *
         FROM sensor_data
-        ORDER BY timestamp DESC
-        LIMIT 5;
+        ORDER BY device_id, sensor_id, timestamp DESC;
     """)
     data = cur.fetchall()
     cur.close()
     conn.close()
     return data
-
-# Split the data for each device ID into its own table
-def split_by_device_id(data):
-    devices = {}
-    for row in data:
-        device_id = row[1]
-        if device_id not in devices:
-            devices[device_id] = []
-        devices[device_id].append(row)
-    return devices
 
 # Format the data for display
 def format_data(data):
@@ -45,27 +73,42 @@ def format_data(data):
     for row in data:
         formatted_row = []
         for i, item in enumerate(row):
-            if i == 3 or i == 4 or i == 6 or i == 7:
-                if item < 20:
-                    formatted_row.append('<span style="color: red;">{}</span>'.format(item))
-                elif item < 40:
-                    formatted_row.append('<span style="color: orange;">{}</span>'.format(item))
-                else:
-                    formatted_row.append('<span style="color: green;">{}</span>'.format(item))
-            else:
-                formatted_row.append(item)
+            formatted_row.append(item)
         formatted_data.append(formatted_row)
     return formatted_data
+
+@app.before_request
+def limit_remote_addr():
+    if request.remote_addr != ALLOWED_HOST:
+        return "You're not allowed to access this resource", 403
 
 # Route for the dashboard
 @app.route('/')
 def dashboard():
     data = get_sensor_data()
-    devices = split_by_device_id(data)
-    formatted_data = {}
-    for device_id, device_data in devices.items():
-        formatted_data[device_id] = format_data(device_data)
-    return render_template('dashboard.html', data=formatted_data)
+    formatted_data = format_data(data)
+    conn = connect_database()
+    # Load data from the PostgreSQL database
+    df = pd.read_sql_query('SELECT * FROM sensor_data ORDER BY timestamp ASC', conn)
+
+    # Create a line plot showing the soil moisture over time for each plant
+    fig1 = px.line(df, x='timestamp', y='soil_pct', color='sensor_name',
+                   title='Soil Moisture over Time')
+    fig1.update_layout(yaxis=dict(range=[0, 100]))
+
+    # Create a line plot showing the battery voltage over time for each plant
+    fig2 = px.line(df, x='timestamp', y='batt_volt', color='sensor_name',
+                   title='Battery Voltage over Time')
+    fig2.update_layout(yaxis=dict(range=[3.0, 4.3]))
+
+    # Convert the plots to HTML format
+    plot1_html = fig1.to_html(full_html=False)
+    plot2_html = fig2.to_html(full_html=False)
+
+    # Render the HTML template with the plots
+    return render_template('dashboard.html', data=formatted_data, is_past_six_hours=is_past_six_hours, plot1=plot1_html, plot2=plot2_html)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=4500, debug=True)
+    app.run(host='0.0.0.0', port=DASHBOARD_PORT, debug=DEBUG)
+    conn.close()
+
